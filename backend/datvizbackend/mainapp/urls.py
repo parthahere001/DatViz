66a7cacc3fb482056c9a12a66be71ccf2ce321f2
Parthahere001 <parthahere001@gmail.com>
2023-12-28 01:59:42 +0530

    [ADD] data retrival from backend

--- backend/datvizbackend/mainapp/urls.py
+++ backend/datvizbackend/mainapp/urls.py
@@ -1,9 +1,9 @@
 # your_project/urls.py
 
 from django.urls import path
-from .views import LoadDataFromJsonView
+from .views import LoadDataFromJsonView, FetchDataView
 
 urlpatterns = [
     # path('load-data/', LoadDataFromJsonView.as_view(), name='load_data'),
-    
+    path('fetch-data/', FetchDataView.as_view(), name='fetch_data'),
 ]
--- backend/datvizbackend/mainapp/views.py
+++ backend/datvizbackend/mainapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 import json
 import datetime
 from django.http import JsonResponse
@@ -10,23 +9,23 @@ from .serializers import ArticleDataSerializer
 
 class LoadDataFromJsonView(View):
     def get(self, request, *args, **kwargs):
-        file_path = "mainapp/jsondata.json"  # Replace with the actual path to your JSON file
+        file_path = "mainapp/jsondata.json"  # Use the relative path to json file
 
         try:
             with open(file_path, 'r') as file:
                 data = json.load(file)
 
                 for item in data:
-                    # Convert string representations of dates to datetime objects
+                    
                     item['added'] = datetime.datetime.strptime(item['added'], '%B, %d %Y %H:%M:%S') if item['added'] else None
                     item['published'] = datetime.datetime.strptime(item['published'], '%B, %d %Y %H:%M:%S') if item['published'] else None
 
-                    # Use the serializer to validate and create the model instance
+                    
                     serializer = ArticleDataSerializer(data=item)
                     if serializer.is_valid():
                         serializer.save()
                     else:
-                        # Handle validation errors
+                        
                         return JsonResponse({'error': f"Error in data: {serializer.errors}"}, status=400)
 
             return JsonResponse({'success': 'Data loaded successfully'}, status=200)
@@ -37,5 +36,15 @@ class LoadDataFromJsonView(View):
         except ValidationError as e:
             return JsonResponse({'error': f"Validation error: {e}"}, status=400)
 
+        except Exception as e:
+            return JsonResponse({'error': f"An unexpected error occurred: {e}"}, status=500)
+
+
+class FetchDataView(View):
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = ArticleData.objects.all()  
+            serializer = ArticleDataSerializer(queryset, many=True)
+            return JsonResponse(serializer.data, safe=False)
         except Exception as e:
             return JsonResponse({'error': f"An unexpected error occurred: {e}"}, status=500)
